d47421d014d30720328a882c65ae5c0896d7fd03
vdk0987 <vdk0987@gmail.com>
2026-03-03 18:35:19 +0530

    sidebar added in dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,16 @@
-const dashboardLayout = ({ children }) => {
-  return <div>{children}</div>;
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+
+const dashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <SidebarProvider>
+      <div className="flex min-h-screen w-full">
+        <AppSidebar />
+
+        <main className="flex-1 bg-background p-6">{children}</main>
+      </div>
+    </SidebarProvider>
+  );
 };
 
 export default dashboardLayout;
