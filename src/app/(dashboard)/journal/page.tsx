ce5b38192175e7027f83e711c0653f37524e1b14
vdk0987 <vdk0987@gmail.com>
2026-05-02 16:37:40 +0530

    Display previous entries

--- src/app/(dashboard)/journal/page.tsx
+++ src/app/(dashboard)/journal/page.tsx
@@ -1,9 +1,21 @@
 import NewEntryCard from "@/components/newEntry";
+import EntryCard from "@/components/entryCard";
+import getEntries from "@/utils/getEntries";
+import Link from "next/link";
 
 const journalPage = async () => {
+  const entries = await getEntries();
   return (
-    <div>
-      <NewEntryCard />
+    <div className="p-10 h-full w-full">
+      <h2 className="text-3xl mb-8">Journal</h2>
+      <div className="grid grid-cols-3 gap-4 ">
+        <NewEntryCard />
+        {entries.map((entry) => (
+          <Link href={`/journal/${entry.id}`} key={entry.id}>
+            <EntryCard entry={entry} />
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -32,7 +32,7 @@ export default function Page({ children }: { children: React.ReactNode }){
           </Breadcrumb>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-5">
+          <div>
             {children}
           </div>
         </div>
